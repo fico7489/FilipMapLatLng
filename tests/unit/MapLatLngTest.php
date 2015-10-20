07f9c718f502ca85ff72090fc9a727ced65c6876
Filip Horvat <fico7489@gmail.com>
2015-10-20 10:14:09 +0200

    add function for distance

--- tests/unit/MapLatLngTest.php
+++ tests/unit/MapLatLngTest.php
@@ -11,6 +11,37 @@
 			$distance = $map->distance(2, 2, 2, 2);
 			
 			$this->assertEquals(null, $distance);
+			
+			
+			$lat = 88.898556;
+			$lng = 67.037852;
+			$lat2 = 38.897147;
+			$lng2 = 77.145;
+			$unit = "K";
+			$distance = $map->distance($lat, $lng, $lat2, $lng2, $unit);
+			$distance = round($distance, 4);
+			$this->assertEquals('5561.566', $distance);
+			
+			$unit = "M";
+			$distance = $map->distance($lat, $lng, $lat2, $lng2, $unit);
+			$distance = round($distance, 4);
+			$this->assertEquals('3455.7969', $distance);
+			
+			$unit = "N";
+			$distance = $map->distance($lat, $lng, $lat2, $lng2, $unit);
+			$distance = round($distance, 4);
+			$this->assertEquals('3001.014', $distance);
+			
+			$lat2 = -38.897147;
+			$unit = "K";
+			$distance = $map->distance($lat, $lng, $lat2, $lng2, $unit);
+			$distance = round($distance, 4);
+			$this->assertEquals('14211.4221', $distance);
+			
+			$unit = "M";
+			$distance = $map->distance($lat, $lng, $lat2, $lng2, $unit);
+			$distance = round($distance, 4);
+			$this->assertEquals('8830.5683', $distance);
 		}
 	}
 
